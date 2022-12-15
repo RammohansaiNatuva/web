12fc47e02c3292b1af8be9b22b8536f928349a37
N.Rammohansai <118331303+RammohansaiNatuva@users.noreply.github.com>
2022-12-15 12:16:47 +0530

    Add files via upload

---
+++ js for resume/js/resume.js
@@ -0,0 +1,44 @@
+function resume(){
+    let firstname;
+    firstname=document.getElementById('firstname').value;
+    let middlename;
+    middlename=document.getElementById('middlename').value;
+    let lastname;
+    lastname=document.getElementById('lastname').value;
+    document.getElementById('name').innerHTML=firstname+middlename+lastname;
+    let Email;
+    Email=document.getElementById('emailId').value;
+    document.getElementById('Email').innerHTML=Email;
+    let number;
+    ph_no=document.getElementById('n').value;
+    document.getElementById('ph_no').innerHTML=ph_no;
+    let school;
+    school=document.getElementById('School').value;
+    document.getElementById('school').innerHTML=school;
+    let passedout;
+    passed_out=document.getElementById('Passed_out').value;
+    document.getElementById('passed_out').innerHTML=passed_out;
+    let results;
+    results=document.getElementById('Results').value;
+    document.getElementById('results').innerHTML=results;
+    let college_name;
+    college_name=document.getElementById('College_name').value;
+    document.getElementById('college_name').innerHTML=college_name;
+    let passing;
+    passing=document.getElementById('Passing').value;
+    document.getElementById('passing').innerHTML=passing;
+    let percentage;
+    percentage=document.getElementById('Percentage').value;
+    document.getElementById('percentage').innerHTML=percentage;
+    let university;
+    university=document.getElementById('University').value;
+    document.getElementById('university').innerHTML=university;
+    let year;
+    year=document.getElementById('Year').value;
+    document.getElementById('year').innerHTML=year;
+    let grade;
+     grade=document.getElementById('Grade').value;
+    document.getElementById('grade').innerHTML=grade;
+
+
+}
